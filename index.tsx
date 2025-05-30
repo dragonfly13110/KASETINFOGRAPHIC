09230b8341eed623dfc6827e1bad5b7f15b128d9
dragonfly13110 <dragonfly.forester@outlook.com>
2025-05-31 00:18:33 +0700

    main

--- index.tsx
+++ index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://dldiedktrkbwpqznxdwk.supabase.co'; // เปลี่ยนเป็น URL ของคุณ
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsZGllZGt0cmtid3Bxem54ZHdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1OTM4ODMsImV4cCI6MjA2NDE2OTg4M30.teFTNA7Zez_qUO9-wQALPdw_ULLmmfNhOlNHD_CdtHc'; // เปลี่ยนเป็น API Key ของคุณ
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
